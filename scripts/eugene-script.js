d8afb9dbfa4fb698edf01c8462849727c5a95cf9
anarchygene <eugenekee@hotmail.sg>
2021-01-20 23:00:16 +0800

    Kinda fixed button(need update ltr)

--- scripts/eugene-script.js
+++ scripts/eugene-script.js
@@ -1,40 +1,45 @@
-function add() {
-    let tracking = `<div class="track">
-    <button class="close" onclick=deletetracking()>X</button>
-    <label for="companyid">Company ID</label>
-    <input type="text" id="companyid" class="companyid">
+var j = 0
+add = () => {
+    let tracking = `<div class="track${j}">
+                        <button class="close${j}" onclick=deletetracking(${j})>X</button>
+                        <label for="companyid">Company ID</label>
+                        <input type="text" id="companyid" class="companyid${j}">
 
-    <button type="submit" class="search" onclick=search()>Search</button><br>
-    <label for="queueid">Queue ID</label>
-    <select name="queueid" id="queueid" class="queueid">
-        <option value="1">hello</option>
-    </select>
+                        <button type="submit" class="search${j}" onclick=searchqueue(${j})>Search</button><br>
+                        <label for="queueid">Queue ID</label>
+                        <select name="queueid" id="queueid" class="queueid${j}">
+                            <option value="1">hello</option>
+                        </select>
 
-    <label for="hide">Hide inactive</label>
-    <input type="checkbox" name="hide" id="hide">
-</div>`
+                        <label for="hide">Hide inactive</label>
+                        <input type="checkbox" name="hide" id="hide">
+                        <canvas id="myChart" width="400" height="400"></canvas>
+                    </div>`
     document.getElementById("add").insertAdjacentHTML("beforebegin", tracking)
-    console.log("Add new tracking")
+    console.log(`Add new tracking ${j}`)
+    j++;
 }
 
-function search() {
-    var companyid = $(".companyid").val();
-    console.log(companyid);
-    var numberofQueue = []
-    $.ajax({
-        url: `http://localhost:8080/company/queue?company_id=${companyid}`,
-        type: 'GET',
-        contentType: "application/json; charset=utf-8",
-        dataType: 'json',
-        success: function (data, textStatus, xhr) {
-            var $dropdown = $(".queueid");
-            $dropdown.empty()
-            for (let i = 0; i < data.length; i++) {
-                console.log(data[i].queue_id);
-                numberofQueue.push(data[i].queue_id)
-                $dropdown.append(`<option value=${numberofQueue[i]}>${numberofQueue[i]}</option>`);
-            }
-            var ctx = document.getElementById('myChart');
+searchqueue = (id) =>{
+    for (let i = 0; i < j; i++) {
+        if (i == id) {
+            var companyid = $(`.companyid${id}`).val();
+            console.log(companyid);
+            var numberofQueue = []
+            $.ajax({
+                url: `http://localhost:8080/company/queue?company_id=${companyid}`,
+                type: 'GET',
+                contentType: "application/json; charset=utf-8",
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    var $dropdown = $(`.queueid${id}`);
+                    $dropdown.empty()
+                    for (let i = 0; i < data.length; i++) {
+                        console.log(data[i].queue_id);
+                        numberofQueue.push(data[i].queue_id)
+                        $dropdown.append(`<option value=${numberofQueue[i]}>${numberofQueue[i]}</option>`);
+                    }
+                    var ctx = document.getElementById('myChart');
                     var myLineChart = new Chart(ctx, {
                         type: 'line',
                         data: {
@@ -66,17 +71,25 @@ function search() {
                             }
                         }
                     });
-        },
-        error: function (xhr, textStatus, errorThrown) {
-            console.log('Error in Operation');
-            alert("Function got error");
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Operation');
+                    alert("Function got error");
+                }
+            })
         }
-    })
+    }
 }
 
-function deletetracking() {
-    $(".close").click(function () {
-        console.log("yeet")
-        $(".track").remove()
-    })
+deletetracking = (id) => {
+    for (let i = 0; i < j; i++) {
+        if (i == id) {
+            console.log(`Removed tracking ${id}`)
+            $(`.track${id}`).remove()
+        }
+    }
+}
+
+check = () => {
+    console.log(`Total number of tracks ${document.querySelectorAll('[class*="track"]').length}`);
 }
